4a4fc47085b780a814db59998078517b045298d3
Tephyz <158828331+Tephyz@users.noreply.github.com>
2026-03-10 15:55:17 +0800

    add ouline modal

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -278,7 +278,7 @@ export default function POSLayout() {
       {/* PRODUCT MODAL */}
       {selectedProduct && (
         <div className="fixed inset-0 bg-white/30 backdrop-blur-sm flex items-center justify-center z-50">
-          <div className="bg-white rounded-lg p-6 w-96 relative">
+          <div className="bg-white rounded-lg p-6 w-96 relative border-2 border-[#5a3e2b]">
             <button
               className="absolute top-2 right-2 text-gray-500 hover:text-gray-800"
               onClick={() => setSelectedProduct(null)}
